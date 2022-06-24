5076c7ecafe84a3114b38de166bcc16e29bf7e20
Owais Athar <69184167+Owais28@users.noreply.github.com>
2022-06-24 18:14:29 +0530

    Initial

---
+++ src/com/learning/Main.java
@@ -0,0 +1,9 @@
+package com.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        float random = (float)Math.random() * 10;
+        System.out.println(random);
+
+    }
+}
